ddbe68e630f63e9f4ac50d8d038104b85c476536
Ivan H. <130370828+noahlevi@users.noreply.github.com>
2025-01-29 12:30:06 +0200

    fix: some db conn improvement

--- db/connection.go
+++ db/connection.go
@@ -1,28 +1,46 @@
 package db
 
 import (
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
-    "log"
-    "user-management/models"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"user-management/models"
 )
 
 var DB *gorm.DB
 
 func Init() {
-    dsn := "host=localhost user=gouser dbname=yourdb password=yourpassword sslmode=disable"
-    var err error
-    DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        log.Fatal("failed to connect to database: ", err)
-    }
-    // Remove the AutoMigrate here
+
+	env_err := godotenv.Load()
+	if env_err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbName)
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect to database: ", err)
+	}
+	// Remove the AutoMigrate here
 }
 
 // Migrate function to run migrations
 func Migrate() {
-    err := DB.AutoMigrate(&models.User{}) // Use the models.User struct
-    if err != nil {
-        log.Fatal("failed to migrate database: ", err)
-    }
+	err := DB.AutoMigrate(&models.User{}) // Use the models.User struct
+	if err != nil {
+		log.Fatal("failed to migrate database: ", err)
+	}
 }
